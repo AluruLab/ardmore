d237556cd3d07223732e239737052c2401a2826d
Sriram P C <sriram.pc@gmail.com>
2019-03-12 12:10:36 -0400

    fixed a typo

--- containers/genie3/test.R
+++ containers/genie3/test.R
@@ -1,13 +1,13 @@
-library(GENEIE3)
+library(GENIE3)
 library(grndata)
 
 
 #expr.matrix <- read.expr.matrix("data.txt", form="rows.are.samples")
-#weight.matrix <- GENIE3(expr.matrix, ncores=16)
+#weight.matrix <- GENIE3(expr.matrix, nCores=16)
 
 yeast.matrix <- getData(datasource.name="gnw2000",getNet=FALSE)
 # TODO: CONVERT FROM GRNDATA TO GENIE3 FORMAT : yeast.matrix
-weight.matrix <- GENIE3(yeast.matrix, ncores=16)
+weight.matrix <- GENIE3(yeast.matrix, nCores=16)
 link.list <- get.link.list(weight.matrix)
 write.table(link.list, file="weights.csv", sep=',', col.names=NA)
 
