d68d74602c510dd3496122c433b99cb0920717b4
Sriram P C <sriram.pc@gmail.com>
2019-03-12 11:48:19 -0400

    update base and genie3 to add bioconductor

--- containers/genie3/test.R
+++ containers/genie3/test.R
@@ -1,7 +1,13 @@
-source("/usr/local/bin/GENIE3.R")
+library(GENEIE3)
+library(grndata)
 
-expr.matrix <- read.expr.matrix("data.txt", form="rows.are.samples")
-weight.matrix <- GENIE3(expr.matrix, ncores=16)
+
+#expr.matrix <- read.expr.matrix("data.txt", form="rows.are.samples")
+#weight.matrix <- GENIE3(expr.matrix, ncores=16)
+
+yeast.matrix <- getData(datasource.name="gnw2000",getNet=FALSE)
+# TODO: CONVERT FROM GRNDATA TO GENIE3 FORMAT : yeast.matrix
+weight.matrix <- GENIE3(yeast.matrix, ncores=16)
 link.list <- get.link.list(weight.matrix)
 write.table(link.list, file="weights.csv", sep=',', col.names=NA)
 
