f684f37575a7598985bda11dc1482d9e362f553f
shruti1995 <shruti.shivakumar@gmail.com>
2019-04-07 16:26:13 -0400

    Added aracne to runs

---
+++ runs/aracne/format.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+unset filename
+while getopts ":f:" opt
+do
+    case $opt in 
+        f ) 
+            filename=$OPTARG
+            ;;
+        \? ) 
+            echo "Usage format.sh [-f filename]"
+            exit
+            ;;
+        : )
+            echo "Invalid option: -$OPTARG requires an argument"
+            exit
+            ;;
+    esac
+done
+shift $((OPTIND -1))
+
+if [[ -z "$filename" ]]
+then
+    echo "Usage format.sh [-f filename]"
+    exit
+fi
+
+ngenes=$(head -n1 $filename | wc -w)
+nsamples=$(tail -n +2 $filename | wc -l)
+mkdir -p aracne/data
+awk '
+{ 
+    for (i=1; i<=NF; i++)  {
+        a[NR,i] = $i
+    }
+}
+NF>p { p = NF }
+END {    
+    for(j=1; j<=p; j++) {
+        str=a[1,j]
+        for(i=2; i<=NR; i++){
+            str=str"\t"a[i,j];
+        }
+        print str
+    }
+}' <(tail -n +2 $filename | cut -d ' ' -f 2-) >aracne/data/temp
+
+paste <(head -n1 $filename | sed 's/"//g' | tr ' ' '\n') aracne/data/temp >aracne/data/temp2
+formattedname=${filename##*/}
+{ echo -n -e "genes\t"; for ((i=1;i<$nsamples;i++)); do echo -n -e "S$i\t"; done; echo "S$nsamples"; cat aracne/data/temp2; } >aracne/data/format.$formattedname
+rm aracne/data/{temp,temp2}
--- runs/aracne/run.sh
+++ runs/aracne/run.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
+#SBATCH --job-name=aracne
+#SBATCH --output=./aracne/logs/log_%J.out
+#SBATCH --error=./aracne/logs/err_%J.err
+#SBATCH --ntasks=64
+#SBATCH --time=24:00:00
 
-sbatch --export=infile='',genes='' slurm.sh
+date
+pwd
+#sbatch --export=infile='',genes='' slurm.sh
+
+ngenes=$(tail -n +2 $datafile | wc -l)
+nexpts=$(head -n 1 $datafile | wc -w)
+((nexpts++))
+
+cd $SLURM_SUBMIT_DIR/aracne
+module load singularity-3.0
+
+mkdir -p aracne/output
+singularity exec im_aracne.sif java -Xmx5G -jar /usr/local/bin/aracne.jar -e $datafile -o output --tfs <(tail -n +2 $datafile | cut -d ' ' -f 1) --pvalue 1E-8 --seed 1 --calculateThreshold
+singularity exec im_aracne.sif java -Xmx5G -jar /usr/local/bin/aracne.jar -e $datafile -o output --tfs <(tail -n +2 $datafile | cut -d ' ' -f 1) --pvalue 1E-8 --seed 1 --threads 64
+
+echo
+date
