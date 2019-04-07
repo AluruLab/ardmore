237245ab8801ddc8e5a32335237f777e81dfa9f1
shruti1995 <shruti.shivakumar@gmail.com>
2019-04-07 16:36:07 -0400

    Minor fix

--- runs/aracne/run.sh
+++ runs/aracne/run.sh
@@ -16,7 +16,7 @@ nexpts=$(head -n 1 $datafile | wc -w)
 cd $SLURM_SUBMIT_DIR/aracne
 module load singularity-3.0
 
-mkdir -p aracne/output
+mkdir -p output
 singularity exec im_aracne.sif java -Xmx5G -jar /usr/local/bin/aracne.jar -e $datafile -o output --tfs <(tail -n +2 $datafile | cut -d ' ' -f 1) --pvalue 1E-8 --seed 1 --calculateThreshold
 singularity exec im_aracne.sif java -Xmx5G -jar /usr/local/bin/aracne.jar -e $datafile -o output --tfs <(tail -n +2 $datafile | cut -d ' ' -f 1) --pvalue 1E-8 --seed 1 --threads 64
 
