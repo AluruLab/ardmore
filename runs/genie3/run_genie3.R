4064914a5d103b9815401f35c7fe193914d564d5
shruti1995 <shruti.shivakumar@gmail.com>
2019-04-08 19:37:20 -0400

    added genie3 to runs

--- runs/genie3/run_genie3.R
+++ runs/genie3/run_genie3.R
@@ -5,6 +5,6 @@ args = commandArgs(trailingOnly=TRUE)
 
 X <- read.table(args[1], header=TRUE, sep=" ", row.names=1)
 expts_data <- data.matrix(X, rownames.force=TRUE)
-weightMatrix <- GENIE3(expts_data, nCores=args[2])
+weightMatrix <- GENIE3(expts_data, nCores=args[3])
 linkList <- getLinkList(weightMatrix)
-write.table(linkList, file="genie3.out")
+write.table(linkList, file=args[2])
