1d12fee5e764afd2d03667123fdeb6fd1bc64bfa
shruti1995 <shruti.shivakumar@gmail.com>
2019-04-08 18:07:57 -0400

    added fastggm to runs

--- runs/startruns.sh
+++ runs/startruns.sh
@@ -4,10 +4,11 @@ while read -r f || [[ -n "$f" ]]
 do
     while read -r m || [[ -n "$m" ]]
     do
-	echo "Method: $m"
-	echo "Dataset: $f"
-	formattedname=${f##*/}
+        echo "Method: $m"
+        echo "Dataset: $f"
+        formattedname=${f##*/}
         /bin/bash $m/format.sh -f $f
+        mkdir -p $m/logs
         sbatch --export=datafile="$m"/data/format."$formattedname" $m/run.sh
     done < methods.config
 done < datafiles.config
