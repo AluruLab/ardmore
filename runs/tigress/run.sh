ac8f32c2f7f93a69db889611e74b5a95859a9960
shruti1995 <shruti.shivakumar@gmail.com>
2019-04-08 21:02:23 -0400

    added tigress to runs

--- runs/tigress/run.sh
+++ runs/tigress/run.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
+#SBATCH --job-name=tigress
+#SBATCH --output=./tigress/logs/log_%J.out
+#SBATCH --error=./tigress/logs/err_%J.err
+#SBATCH --ntasks=64
+#SBATCH --time=24:00:00
 
-sbatch --export=infile='' slurm.sh
+date
+pwd
+
+nexpts=$(tail -n +2 $datafile | wc -l)
+ngenes=$(head -n 1 $datafile | wc -w)
+echo "Number of genes : $ngenes"
+echo "Number of expts : $nexpts" 
+
+module load singularity-3.0
+
+mkdir -p tigress/output
+
+# Rscript --vanilla run_tigress.R <data file> <output file> <number of cores>
+currenttime=$(date "+%Y.%m.%d-%H.%M.%S")
+singularity exec $PWD/tigress/im_tigress.sif \
+	Rscript --vanilla $PWD/tigress/run_tigress.R \
+	$PWD/$datafile $PWD/tigress/output/${ngenes}.${nexpts}-${currenttime} \
+	64
+
+echo
+date
