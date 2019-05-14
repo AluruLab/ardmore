8c7da39c627ac69b272b4177a069685fe41e0d4d
Sriram P C <sriram.pc@gmail.com>
2019-05-14 13:32:14 -0400

    added scripts for running grnboost

--- containers/grnboost/test_grnb.sh
+++ containers/grnboost/test_grnb.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#SBATCH -n 64
+#SBATCH -t 12:00:00
+#SBATCH -o grnboost.log
+#SBATCH -e grnboost.log
+
+module load singularity-3.0
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 # export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/
@@ -6,19 +12,20 @@ export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 # ./build.sh
 # singularity pull docker://tanyard/im:grnboost
 # singularity run im_grnboost.sif mvn -version
-
-singularity run im_grnboost.sif /usr/local/bin/spark/bin/spark-submit \
+#     --total-executor-cores 7 \
+/usr/bin/time -f "%e %U %S %P" \
+     singularity run im_grnboost.sif /usr/local/bin/spark/bin/spark-submit \
      --class org.aertslab.grnboost.GRNBoost \
-     --num-executors 4 \
-     --executor-cores 1 \
-     --master local[*] \
+     --driver-memory 32g \
+     --executor-memory 8g \
+     --master local[64] \
      --deploy-mode client \
      --jars /m2/repo/ml/dmlc/xgboost4j/0.83-SNAPSHOT/xgboost4j-0.83-SNAPSHOT.jar \
       /work/GRNBoost/target/scala-2.11/GRNBoost.jar \
      infer \
-     -i ./toy-genesXexp-tab-grnboost.tsv \
-     -tf ./toy-tfs-grnboost.tsv \
-     -o ./grnboost-net \
+     -i ./exp-data-2k.tsv \
+     -tf ./genes \
+     -o ./grnboost-net-small \
      -p eta=0.01 \
      -p max_depth=3 \
      -p colsample_bytree=0.1 \
--- containers/grnboost/test_small.sh
+++ containers/grnboost/test_small.sh
@@ -6,19 +6,19 @@ export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 # ./build.sh
 # singularity pull docker://tanyard/im:grnboost
 # singularity run im_grnboost.sif mvn -version
-
-singularity run im_grnboost.sif /usr/local/bin/spark/bin/spark-submit \
+#     --total-executor-cores 7 \
+/usr/bin/time -f "%e %t %P %K"  singularity run im_grnboost.sif /usr/local/bin/spark/bin/spark-submit \
      --class org.aertslab.grnboost.GRNBoost \
-     --num-executors 4 \
-     --executor-cores 1 \
-     --master local[*] \
+     --driver-memory 32g \
+     --executor-memory 8g \
+     --master local[16] \
      --deploy-mode client \
      --jars /m2/repo/ml/dmlc/xgboost4j/0.83-SNAPSHOT/xgboost4j-0.83-SNAPSHOT.jar \
       /work/GRNBoost/target/scala-2.11/GRNBoost.jar \
      infer \
-     -i ./toy-genesXexp-tab-grnboost.tsv \
-     -tf ./toy-tfs-grnboost.tsv \
-     -o ./grnboost-net \
+     -i ./exp-data.tsv \
+     -tf ./genes \
+     -o ./grnboost-net-small \
      -p eta=0.01 \
      -p max_depth=3 \
      -p colsample_bytree=0.1 \
