645c98b02729c1cc858232b4df515df5ec6263af
Shruti Shivakumar <sshivakumar9@gatech.edu>
2019-05-15 16:18:39 -0400

    Added precision scores and pr plots to fastggm

--- eval/roc_fastggm.py
+++ eval/roc_fastggm.py
@@ -1,7 +1,7 @@
 import numpy, pandas, os, errno, matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
-from sklearn.metrics import roc_auc_score, roc_curve
+from sklearn.metrics import roc_curve, roc_auc_score, average_precision_score, precision_recall_curve
 
 try:
     os.mkdir("fastggm")
@@ -11,33 +11,52 @@ except OSError as e:
 
 true_complete = pandas.read_csv('../data/yeast/gnw2000_truenet', sep=' ', index_col=0)
 
-print('AUROC')
-for _filename in os.listdir('../runs/fastggm/output'):
+files = os.listdir('../runs/fastggm/output')
+auroc = []
+average_precision = []
+for _filename in files:
     filename = '../runs/fastggm/output/' + _filename
-    ngenes = _filename.split('.', 1)[0]
-    nsamples = _filename.split('.', 1)[1].split('-',1)[0]
+    ngenes = int(_filename.split('.', 1)[0])
+    nsamples = int(_filename.split('.', 1)[1].split('-',1)[0])
     datafile = '../runs/fastggm/data/format.gnwn' + str(nsamples) + 'X' + str(ngenes)
     genes = []
     with open(datafile) as f:
         line = f.readline().replace('"', '')
         genes = line.split()
     
-    pred = pandas.read_csv(filename, sep=' ', index_col=0, usecols=range(int(ngenes)))
+    pred = pandas.read_csv(filename, sep=' ', index_col=0, usecols=range(2*ngenes, 3*ngenes))
     true = true_complete.loc[genes, genes]
     pred = pred.values.flatten()
     true = true.values.flatten()
     
     fpr, tpr, thresholds = roc_curve(true, pred)
-    auroc = roc_auc_score(true, pred)
+    precision, recall, _ = precision_recall_curve(true, pred)
+    auroc.append(roc_auc_score(true, pred))
+    average_precision.append(average_precision_score(true, pred))
     
     fig = plt.figure()
+
+    plt.subplot(1, 2, 1)
     plt.plot(fpr, tpr, color='darkorange')
     plt.plot([0,1], [0,1], color='navy', linestyle='--')
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
-    fig.savefig('fastggm/'+_filename+'.png')
 
-    print(filename + " : " + str(auroc))
+    plt.subplot(1, 2, 2)
+    plt.plot(recall, precision, color='darkorange')
+    plt.plot([0, 1], [1, 0], color='navy', linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
 
+    fig.savefig('fastggm/'+_filename.split('-')[0]+'_roc.png')
+
+print("AUROC")
+for i in range(len(auroc)):
+    print(files[i] + " : " + str(auroc[i]))
+print("Average_precision")
+for i in range(len(average_precision)):
+    print(files[i] + " : " + str(average_precision[i]))
